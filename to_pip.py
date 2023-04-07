3c6e1d7946c34d1845dc335a406c5cf9b5a08c0e
chiubowen <cbh@cameo.tw>
2023-04-07 21:44:27 +0800

    v1.4 add:     python -m pip install to-pip     python -m to_pip

--- to_pip.py
+++ to_pip.py
@@ -7,32 +7,32 @@ import tempfile
 
 def usage():
     print(
-        f"Usage: {sys.argv[0]} -n <package_name> -v <package_version> [-u <pypi_username> -p <pypi_password>] <python_files>"
+        f"Usage: python -m to_pip -n <package_name> -v <package_version> [-u <pypi_username> -p <pypi_password>] <python_files>"
     )
     sys.exit(1)
 
 
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument("-n", "--name", help="Package name", required=True)
-    parser.add_argument("-v", "--version", help="Package version", required=True)
-    parser.add_argument("-u", "--username", help="PyPI username", default="")
-    parser.add_argument("-p", "--password", help="PyPI password", default="")
-    parser.add_argument("files", nargs="*", help="Python files to include")
+    parser.add_argument("-n", "--package_name", help="Package name", required=True)
+    parser.add_argument("-v", "--package_version", help="Package version", required=True)
+    parser.add_argument("-u", "--pypi_username", help="PyPI username", default="")
+    parser.add_argument("-p", "--pypi_password", help="PyPI password", default="")
+    parser.add_argument("python_files", nargs="*", help="Python files to include")
     return parser.parse_args()
 
 
 def to_pip():
     args = parse_args()
 
-    if not args.files:
+    if not args.python_files:
         usage()
 
     tmp_dir = tempfile.mkdtemp()
-    package_dir = os.path.join(tmp_dir, f"{args.name}-{args.version}")
+    package_dir = os.path.join(tmp_dir, f"{args.package_name}-{args.package_version}")
     os.makedirs(package_dir)
 
-    for file in args.files:
+    for file in args.python_files:
         with open(file) as src, open(
                 os.path.join(package_dir, os.path.basename(file)), "w"
         ) as dest:
@@ -43,11 +43,11 @@ def to_pip():
     if os.path.exists("requirements.txt"):
         shutil.copy("requirements.txt", os.path.join(package_dir, "requirements.txt"))
 
-    modules = ", ".join([f"'{os.path.basename(file).split('.')[0].replace('-', '_')}'" for file in args.files])
+    modules = ", ".join([f"'{os.path.basename(file).split('.')[0].replace('-', '_')}'" for file in args.python_files])
     entry_points = ", ".join(
         [
             f"{os.path.basename(file).split('.')[0].replace('-', '_')} = {os.path.basename(file).split('.')[0].replace('-', '_')}:main"
-            for file in args.files
+            for file in args.python_files
         ]
     )
 
@@ -58,8 +58,8 @@ with open('requirements.txt') as f:
     requirements = [line.strip() for line in f.readlines()]
 
 setup(
-    name="{args.name}",
-    version="{args.version}",
+    name="{args.package_name}",
+    version="{args.package_version}",
     packages=find_packages(),
     py_modules=[{modules}],
     install_requires=requirements,
@@ -80,7 +80,7 @@ setup(
 
     os.system(f"cd {package_dir} && python setup.py sdist bdist_wheel")
 
-    if args.username and args.password:
+    if args.pypi_username and args.pipy_password:
         pypirc_content = f"""
 [distutils]
 index-servers =
@@ -88,8 +88,8 @@ index-servers =
 
 [pypi]
 repository: https://upload.pypi.org/legacy/
-username: {args.username}
-password: {args.password}
+username: {args.pipy_username}
+password: {args.pipy_password}
 """
         with open(os.path.expanduser("~/.pypirc"), "w") as f:
             f.write(pypirc_content)
