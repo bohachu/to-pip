1550312549d8d0a68c36ce0684bfbeebb505b57f
chiubowen <cbh@cameo.tw>
2023-04-08 09:08:58 +0800

    2023-04-08 09:08 Bowen: 帥！已經可以 to_run.sh 到 google cloud run 自動化

--- to_run.sh
+++ to_run.sh
@@ -1,14 +1,14 @@
 docker build --platform linux/amd64 -t to-pip .
-docker tag to-pip gcr.io/falra-368206/to-pip:1.2
-docker push gcr.io/falra-368206/to-pip:1.2
+docker tag to-pip gcr.io/falra-368206/to-pip:1.9.0
+docker push gcr.io/falra-368206/to-pip:1.9.0
 gcloud config set run/region us-central1
 gcloud run deploy to-pip \
---image=gcr.io/falra-368206/to-pip:1.2
+--image=gcr.io/falra-368206/to-pip:1.9.0 \
 --allow-unauthenticated \
 --port=8000 \
 --service-account=1037695533427-compute@developer.gserviceaccount.com \
 --concurrency=1 \
---cpu=0.1 \
+--cpu=0.08 \
 --memory=128Mi \
 --max-instances=1 \
 --region=us-central1 \
