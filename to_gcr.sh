798cbb510dd583144abd1370e9f78570f2b5b45d
chiubowen <cbh@cameo.tw>
2023-04-08 08:46:41 +0800

    v1.9.0     修改讓 html 引用     <script src="https://cdn.jsdelivr.net/gh/bohachu/default-input@v1.1.0/defaultInput.js"></script>

--- to_gcr.sh
+++ to_gcr.sh
@@ -1,9 +1,7 @@
 docker build --platform linux/amd64 -t to-pip .
 docker tag to-pip gcr.io/falra-368206/to-pip:1.2
 docker push gcr.io/falra-368206/to-pip:1.2
-
 gcloud config set run/region us-central1
-
 gcloud run deploy to-pip \
 --image=gcr.io/falra-368206/to-pip:1.2
 --allow-unauthenticated \
